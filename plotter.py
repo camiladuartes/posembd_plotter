ca98e926aea3836748c162ebb5d01b68702cbbf2
camiladuartes <camiladuartes_@ufrn.edu.br>
2020-05-04 21:04:46 -0300

    This creates the option to show the centroid by POS and closes #6

--- plotter.py
+++ plotter.py
@@ -21,6 +21,7 @@ import time
 from tqdm import tqdm
 
 import pyautogui
+from tsne_pos import centroid
 
 def parseQueries(queries, vocab, tag2id):
     ## Different queries are separeted with a ' '
@@ -29,10 +30,16 @@ def parseQueries(queries, vocab, tag2id):
     ## Structure where queries will be saved
     queriesDict = {}
 
+    boolCentroid = False
     for query in queries:
         ## Retrieve token and its dataset+POS
         token, datasetPos = query.split('/')
 
+        ## Check if we want centroid or not
+        if query == queries[len(queries)-1]:
+            if token == "centroidOn":
+                boolCentroid = True
+                break
         ## Retrieve token ID from saved vocab
         tokenId = vocab[token]
 
@@ -52,7 +59,7 @@ def parseQueries(queries, vocab, tag2id):
             else:
                 queriesDict[tokenId] = [(dataset, pos)]
 
-    return queriesDict
+    return queriesDict, boolCentroid
 
 
 def getInfosToPlot(queriesDict, infos, columnDict, wordIdList, id2tag):
@@ -97,7 +104,6 @@ def getInfosToPlot(queriesDict, infos, columnDict, wordIdList, id2tag):
             tsnes = [(info[columnDict['tsne_{}_0'.format(i)]], info[columnDict['tsne_{}_1'.format(i)]])
                                     for i in range(4)]
             infosToPlot.append((wordForm, (goldPOS, predPOS), info[columnDict['dataset']], tsnes))
-
     return infosToPlot, infosToPlotColumnDict
 
 
@@ -124,10 +130,11 @@ def plotter(infos, columnDict, vocab, wordIdList, tagDicts):
 
     while True:
         print('Query. Separate different queries with \' \'. Use * for all possible entries.')
+        print('Type "centroidOn/*" at the end to plot the centroid of representations by POS.')
         queries = input()
         plt.close('all')
         # try:
-        queriesDict = parseQueries(queries, vocab, tag2id)
+        queriesDict, boolCentroid = parseQueries(queries, vocab, tag2id)
         # except:
             # continue
         infosToPlot, infosToPlotColumnDict = getInfosToPlot(queriesDict, infos, columnDict, wordIdList, id2tag)
@@ -144,6 +151,41 @@ def plotter(infos, columnDict, vocab, wordIdList, tagDicts):
             x = [infoToPlot[infosToPlotColumnDict['tsnes']][i][0] for infoToPlot in infosToPlot]
             y = [infoToPlot[infosToPlotColumnDict['tsnes']][i][1] for infoToPlot in infosToPlot]
 
+            ### Centroid
+            if boolCentroid == True:
+                ## Preparing dict to pass through centroid function
+                dict_cent = {}
+                for infoToPlot in infosToPlot:
+                    x = infoToPlot[infosToPlotColumnDict['tsnes']][i][0]
+                    y = infoToPlot[infosToPlotColumnDict['tsnes']][i][1]
+                    word = infoToPlot[infosToPlotColumnDict['word']]
+                    pos = infoToPlot[infosToPlotColumnDict['pos']]
+                    if (word, pos) in dict_cent.keys():
+                        dict_cent[(word, pos)].append([x, y])
+                    else:
+                        dict_cent[(word, pos)] = [x, y]
+
+                ## Passing the dictionary with the word and its pos with their respective tsnes to the function
+                centroids = []
+                for wordPos, tsnesByPos in dict_cent.items():
+                    # If there's more than one occurrence (list of lists of tsnes)
+                    if all(isinstance(t, list) for t in tsnesByPos):
+                        x_ = [t[0] for t in tsnesByPos]
+                        y_ = [t[1] for t in tsnesByPos]
+                    else:
+                        x_ = tsnesByPos[0]
+                        y_ = tsnesByPos[1]
+                    word = wordPos[0]
+                    pos = wordPos[1]
+                    centroids.append(centroid(word, pos, x_, y_))
+
+                ## Assigning x's and y's to plot
+                x = []
+                y = []
+                for c in centroids:
+                    x.append(c[1][0])
+                    y.append(c[1][1])
+
             # Turning pixels into inches (divided by 2 because there are 4 figures)
             figs_x = ((w/2)-55) * 0.010416666666819
             figs_y = ((h/2)-55) * 0.010416666666819
@@ -151,18 +193,30 @@ def plotter(infos, columnDict, vocab, wordIdList, tagDicts):
             fig_.append(fig)
             fig.canvas.manager.window.move(window_[i][0], window_[i][1])
 
-            colors = [float(hash(infoToPlot[infosToPlotColumnDict['word']]) % 256) / 256
-                                for infoToPlot in infosToPlot]
+            if boolCentroid:
+                # c[0][0] here is each word in centroids
+                colors = [float(hash(c[0][0]) % 256) / 256 for c in centroids]
+            else:
+                colors = [float(hash(infoToPlot[infosToPlotColumnDict['word']]) % 256) / 256
+                                    for infoToPlot in infosToPlot]
             plt.scatter(x, y, alpha=1, c=colors, cmap=cm.brg, edgecolors=[(0.0, 0.0, 0.0, 1.0) for _ in colors])
 
             ax = plt.gca()
             ax.set_xlim(lims[i][0])
             ax.set_ylim(lims[i][1])
 
-            for i, infoToPlot in enumerate(infosToPlot):
-                word = infoToPlot[infosToPlotColumnDict['word']]
-                pos = infoToPlot[infosToPlotColumnDict['pos']]
-                plt.annotate(" {} {}".format(word, pos), (x[i], y[i]))
+            if boolCentroid:
+                for c in centroids:
+                    word = c[0][0]
+                    pos = c[0][1]
+                    x = c[1][0]
+                    y = c[1][1]
+                    plt.annotate(" {} {}".format(word, pos), (x, y))
+            else:
+                for j, infoToPlot in enumerate(infosToPlot):
+                    word = infoToPlot[infosToPlotColumnDict['word']]
+                    pos = infoToPlot[infosToPlotColumnDict['pos']]
+                    plt.annotate(" {} {}".format(word, pos), (x[j], y[j]))
             plt.title(plotTitle)
 
         print(">> Press \"q\" to close all figures.")
